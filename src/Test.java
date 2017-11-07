6ef8aae275d6f9b2c507acefacd1b97ac19666d5
laveh2107 <laveh2107@HRH-IC0027329.wrdsb.ca>
2017-11-07 10:04:31 -0500

    Nov 7

--- src/Test.java
+++ src/Test.java
@@ -15,14 +15,19 @@ public class Test {
     public static void main(String[] args) {
         OrderedList list  = new OrderedList();
         list.add(2);
+        System.out.println("TEst");
         for (int i = 0; i < list.size(); i++) {
             System.out.println(list.get(i));
         }
        list.add(1);
+       System.out.println("TEst");
+       
        for (int i = 0; i < list.size(); i++) {
             System.out.println(list.get(i));
         }
         list.add(1);
+        System.out.println("TEst");
+        
         for (int i = 0; i < list.size(); i++) {
             System.out.println(list.get(i));
         }
