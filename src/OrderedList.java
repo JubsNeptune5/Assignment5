0c5daf85a58842a894ba15698db46b47b5090ed8
laveh2107 <laveh2107@HRH-IC0037387.wrdsb.ca>
2017-11-09 11:19:48 -0500

    MSIP nov 9

--- src/OrderedList.java
+++ src/OrderedList.java
@@ -26,23 +26,40 @@ public class OrderedList {
         if (node == null) {
             head = temp;
 
+        } else if (num < head.getNum()) {
+            IntNode p = head;
+            head = temp;
+            System.out.println("Head: "+head.getNum());
+            head.setNext(p);
+            System.out.println("head next: "+head.getNext().getNum());
         } else {
-            if (num < head.getNum()) {
-                IntNode p = head;
-                head = temp;
-                head.setNext(p);
+            while (num > node.getNum()) {
+                System.out.println("num: "+num);
+                System.out.println("Node: "+node.getNum());
+                node = node.getNext();
+                if (node.getNext() == null) {
+                    break;
+                }
             }
-//            //travel to the end
-//            while (node.getNum() < num) {
+        }
+//        } else if (num > head.getNum()) {
+//            while (node.getNext().getNum() < num) {  //NOTE Temp is num/node is<num
+//                if (node.getNext() == null) {
+//                    node.setNext(temp);
+//                    break;
+//                }
 //                //travel to the next node
 //                node = node.getNext();
 //            }
-////            // Node is the last node on the list
-////            IntNode temp2 = new IntNode(num);
-//            //Insert it into the list
-//            node.setNext(temp);
+//        }
+//        //travel to the end
 //
-       }
+//         Node is the last node on the list
+        IntNode temp2 = new IntNode(num);
+//        Insert it into the list
+        temp.setNext(temp2);
+
+
 
         //Increase the size counter
         numItems++;
--- src/Test.java
+++ src/Test.java
@@ -15,22 +15,29 @@ public class Test {
     public static void main(String[] args) {
         OrderedList list  = new OrderedList();
         list.add(2);
-        System.out.println("TEst");
-        for (int i = 0; i < list.size(); i++) {
-            System.out.println(list.get(i));
-        }
+//        System.out.println("TEst");
+//        for (int i = 0; i < list.size(); i++) {
+//            System.out.println(list.get(i));
+//        }
        list.add(1);
+//       System.out.println("TEst");
+//       
+//       for (int i = 0; i < list.size(); i++) {
+//            System.out.println(list.get(i));
+//        }
+        list.add(4);
+//        System.out.println("TEst");
+//        
+//        for (int i = 0; i < list.size(); i++) {
+//            System.out.println(list.get(i));
+//        }
+//        
+         list.add(1);
        System.out.println("TEst");
-       
-       for (int i = 0; i < list.size(); i++) {
-            System.out.println(list.get(i));
-        }
-        list.add(1);
-        System.out.println("TEst");
-        
+//        
         for (int i = 0; i < list.size(); i++) {
             System.out.println(list.get(i));
         }
-        
+//        
     }
 }
