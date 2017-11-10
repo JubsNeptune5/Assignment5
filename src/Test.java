1472fa03ae9d5d0db2bd60e956c507f18790e8d1
laveh2107 <laveh2107@HRH-IC0027288.wrdsb.ca>
2017-11-10 12:39:42 -0500

    Lunch Nove 10

--- src/Test.java
+++ src/Test.java
@@ -13,81 +13,38 @@ public class Test {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        OrderedList list  = new OrderedList();
+        OrderedList list = new OrderedList();
         System.out.println("Question 1");
         System.out.println("");
+        list.add(2);
+        list.add(3);
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+
+
+
+
+
+
+
         A5Q2 array = new A5Q2();
         System.out.println("");
         System.out.println("Question 2");
         System.out.println("");
-        array.add(0,0);
+        array.add(0, 0);
         array.add(1, 6);
-        array.add(6,5);
+        array.add(6, 5);
         for (int i = 2; i < 6; i++) {
-           array.add(i,0); 
+            array.add(i, 0);
         }
         for (int i = 7; i < 32; i++) {
-            array.add(i,0);
+            array.add(i, 0);
         }
-        
-        array.add(33,7);
+
+        array.add(33, 7);
 //        
         array.get();
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-//        list.add(2);
-////        System.out.println("TEst");
-////        for (int i = 0; i < list.size(); i++) {
-////            System.out.println(list.get(i));
-////        }
-//       list.add(1);
-////       System.out.println("TEst");
-////       
-////       for (int i = 0; i < list.size(); i++) {
-////            System.out.println(list.get(i));
-////        }
-//        list.add(4);
-////        System.out.println("TEst");
-////        
-////        for (int i = 0; i < list.size(); i++) {
-////            System.out.println(list.get(i));
-////        }
-////        
-//         list.add(1);
-//       System.out.println("TEst");
-////        
-//        for (int i = 0; i < list.size(); i++) {
-//            System.out.println(list.get(i));
-//        }
-//        
+
     }
 }
