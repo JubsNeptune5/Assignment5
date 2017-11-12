69e7b78f718cabb19c469084acfcd3151992addb
hunterlaverty <hunterlaverty@192.168.0.20>
2017-11-11 23:18:39 -0500

    Home Nov 11

--- src/OrderedList.java
+++ src/OrderedList.java
@@ -11,12 +11,12 @@ public class OrderedList {
 
     private IntNode head;
     private int numItems;
-    private int[] items;
-
+/**
+ *
+ */
     public OrderedList() {
         head = null;
         numItems = 0;
-        items = new int[numItems + 1];
     }
 
     /**
@@ -25,82 +25,107 @@ public class OrderedList {
      * @param num the number put into the list
      */
     public void add(int num) {
-
-        //Statr at beginning of the list
+        //Create node for the head and the number
         IntNode node = head;
         IntNode temp = new IntNode(num);
-        //See if first item is at the start
+        //Check if this is the first number to be in the list
         if (node == null) {
-            System.out.println("TEST 5");
             head = temp;
-        } else {
+        } else{
+            //Check if the number issmaller than the head
             if (num < head.getNum()) {
-                System.out.println("TEST@");
+                //Create a seperate node for the value of the head
                 IntNode p = head;
+                //Switch the number to be the head and set the number back a row
                 head = temp;
-                System.out.println("Head: " + head.getNum());
                 head.setNext(p);
-                System.out.println("head next: " + head.getNext().getNum());
             } else {
+                //check if there is a next number in the list
                 if (node.getNext() == null) {
-                    System.out.println("TWST1");
-                    System.out.println("Temp; " + temp.getNum());
-
+                    //set the next node to be the temp
                     node.setNext(temp);
-                    System.out.println("Node.setNext(temp)" + node.getNext().getNum());
                 } else {
-                    System.out.println("TEST");
+                    //while the next node is less than the number
                     while (node.getNext().getNum() < num) {
+                        //move the value of node up the places tilll the loop is false
                         node = node.getNext();
+                        //Check if the lsit has reached it's last place
                         if (node.getNext() == null) {
                             break;
                         }
                     }
-//        Insert it into the list
+                    //Insert it into the list
                     IntNode p = node.getNext();
                     temp.setNext(p);
                     node.setNext(temp);
                 }
             }
-        p
+            
         }
         //Increase the size counter
         numItems++;
     }
-
+/**
+ * State the size of the list
+ * @return Size of the lsit
+ */
     public int size() {
         return numItems;
     }
-
+/**
+ * check if the list is empty
+ * @return boolean weither or not its empty
+ */
     public boolean isEmpty() {
-        return numItems == 0;
+        if(numItems==0){
+            return true;
+        }else{
+            return false;
+        }
     }
-
+/**
+ * Find the value at a position of the list
+ * @param index the position
+ * @return The value at the position
+ */
     public int get(int index) {
         IntNode node = head;
-        //move the number of times 
+        //Go throught the positions before the position needed 
         for (int i = 0; i < index - 1; i++) {
             node = node.getNext();
         }
+        //return the value
         return node.getNum();
     }
-
+/**
+ * Method to remove the firstnumber that is givin in the list
+ * @param num the number wanting to get rid of
+ */
     public void remove(int num) {
+        //Create nodes to represent the values of head and null
         IntNode temp = head;
         IntNode prev = null;
+        //Check if the temperary node has a value
         while (temp != null) {
+            //Check if the head is the number
             if(temp.getNum() == num){
+                //Check if the temp is the head
                 if(temp ==head){
+                    //Make the value ofhead equal to the value after that
                     head=head.getNext();
                 }else{
+                    //Set the next value of the previous node to the value after the head
                     prev.setNext(temp.getNext());
                 }
                 
             }else{
+                //make the previous equal the temp
                 prev=temp;
             }
+            //Make the temperary equal the value after that
             temp = temp.getNext();
         }
+        //Lower the number in the list
         numItems--;
     }
 }
